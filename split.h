a9c6922bcb0e3093f5a3df4ae48f4375b40e0dda
Giuko <56999735+Giuko@users.noreply.github.com>
2022-02-28 22:43:03 +0100

    Add files via upload

---
+++ split.h
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS			//Only for VS
+#define LENGHT_STRING 128
+#define MAX_WORD 20
+
+#include <stdio.h>
+#include <string.h>
+
+int split(char separator, char string[LENGHT_STRING], char arr[MAX_WORD][LENGHT_STRING]) {
+	unsigned int i = 0, k = 0, j = 0;
+	char word[LENGHT_STRING];
+	while (i < strlen(string) && j < MAX_WORD) {
+		while (string[i] != interruttore && i < strlen(string)) {
+			arr[j][k++] = string[i++];
+		}
+		arr[j++][k] = '\0';
+		k = 0;
+		i++;
+
+		printf("\n");
+	}
+	return j;
+}
+
